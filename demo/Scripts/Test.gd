3a475b0abf3b9143f835467a9ec743d9389967b2
xynanlee <xynanlee@gmail.com>
2026-01-31 18:43:12 +0800

    Update Test.gd

--- demo/Scripts/Test.gd
+++ demo/Scripts/Test.gd
@@ -3,5 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	GDGDK.initialize_gdk()
+	GDGDK.initialize_gdk("")
+	
+	var block: GDGDK_XAsyncBlock = GDGDKUser.add_user_async(GDGDKUser.AddDefaultUserSilently)
+	print(await block.completed)
 	pass # Replace with function body.
